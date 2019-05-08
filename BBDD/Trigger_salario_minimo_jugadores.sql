0d920e7f7e823a68d8dde561f68f47fbf672d6d8
Miguel <miguel.barros@ikasle.egibide.org>
2019-05-08 13:56:25 +0200

    Triggers

---
+++ BBDD/Trigger_salario_minimo_jugadores.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE TRIGGER TRIG_SAL_MIN_JUGADOR --Trigger para que el sueldo sea mayor que el SMI
+BEFORE INSERT OR UPDATE 
+ON JUGADOR 
+FOR EACH ROW
+BEGIN
+    IF PAQUETE_MUTANTE.SUELDOJUGADOR < 736 THEN --Si el sueldo es menor que el SMI
+         RAISE_APPLICATION_ERROR('-20001','No puede tener un sueldo menor que el Sueldo minimo profesional'); --Ejecutar el raise application error
+    END IF; 
+END TRIG_SAL_MIN_JUGADOR;
+
+ALTER TRIGGER TRIG_SAL_MIN_JUGADOR DISABLE;
