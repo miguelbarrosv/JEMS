20dbfe38c951a52f1af51dfc3e100cb22f51d36d
Joel <joel.encinas@ikasle.egibide.org>
2019-05-13 12:43:22 +0200

    Java BD + Diagrama de clases

---
+++ Reto/JEMS/src/BD/JugadorBD.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BD;
+
+/**
+ * Clase de cuentas de base de datos
+ *
+ * @author Joel Encinas
+ * 
+ * @see AdministradorBD
+ * 
+ * @version %I%, %G%
+ * @since 1.0
+ * 
+ * 
+ * 
+ */
+public class JugadorBD {
+    
+}
