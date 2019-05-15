dc20829fa3e19596038246ab363db1faa3317875
eric <eric.munoz@ikasle.egibide.org>
2019-05-15 13:27:21 +0200

    java uml

--- Reto/JEMS/src/UML/Administrador.java
+++ Reto/JEMS/src/UML/Administrador.java
@@ -7,16 +7,87 @@ package UML;
 
 /**
  * @author Sergio Zulueta
+ * @author Eric Muñoz
  *
  * @version %I%, %G%
  * @since 1.0
  */
 public class Administrador{
     /**
-     * Creacion de los atributos cod_admin
+     * Creacion de los atributos cod_admin, usuario y contraseña.
      */
     private int cod_admin;
     private String usuario;
     private String contraseña;
+    
+    /**
+     * Constructor vacio. (Para invocación por constructores de subclases,
+     * típicamente implícito.)
+     */
+    public Administrador() {
+    }
+    /**
+     * Constructor lleno con todos atributos del administrador.
+     * 
+     * @param cod_admin (requerido) El codigo del administrador.
+     * @param usuario (requerido) El usuario del administrador.
+     * @param contraseña (requerido) La contraseña del administrador.
+     */
+    public Administrador(int cod_admin, String usuario, String contraseña) {
+        this.cod_admin = cod_admin;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+    /**
+     * Es un Get que devuelve el codigo del administrador.
+     * 
+     * @return cod_admin devuelve el codigo del administrador
+     */
+    public int getCod_admin() {
+        return cod_admin;
+    }
+    /**
+     * Es un Set que establece el codigo del administrador.
+     * 
+     * @param cod_admin establece el codigo del administrador
+     */
+    public void setCod_admin(int cod_admin) {
+        this.cod_admin = cod_admin;
+    }
+    /**
+     * Es un Get que devuelve el usuario del administrador.
+     * 
+     * @return cod_admin devuelve el usuario del administrador
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+    /**
+     *Es un Set que establece el codigo del administrador.
+     * 
+     * @param usuario establece el usuario del administrador
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    /**
+     * Es un Get que devuelve la contraseña del administrador.
+     * 
+     * @return cod_admin devuelve la contraseña del administrador
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+    /**
+     * Es un Set que establece el codigo del administrador.
+     * 
+     * @param contraseña establece la contraseña del administrador
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+    
     
 }
--- Reto/JEMS/src/UML/Usuario.java
+++ Reto/JEMS/src/UML/Usuario.java
@@ -7,10 +7,84 @@ package UML;
 
 /**
  * @author Sergio Zulueta
+ * @author Eric Muñoz
  *
  * @version %I%, %G%
  * @since 1.0
  */
 public class Usuario{
+    /**
+     * Creacion de los atributos cod_usuario, usuario y contraseña.
+     */
+    private int cod_usuario;
+    private String usuario;
+    private String contraseña;
+    
+    /**
+     * Constructor vacio. (Para invocación por constructores de subclases,
+     * típicamente implícito.)
+     */
+    public Usuario() {
+    }
+    /**
+     * Constructor lleno con todos atributos del usuario.
+     * 
+     * @param cod_usuario (requerido) El codigo del usuario.
+     * @param usuario (requerido) El usuario del usuario.
+     * @param contraseña (requerido) La contraseña del usuario.
+     */
+    public Usuario(int cod_usuario, String usuario, String contraseña) {
+        this.cod_usuario = cod_usuario;
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+    }
+    /**
+     * Es un Get que devuelve el codigo del usuario.
+     * 
+     * @return cod_admin devuelve el codigo del usuario
+     */
+    public int getCod_usuario() {
+        return cod_usuario;
+    }
+    /**
+     * Es un Set que establece el codigo del usuario.
+     * 
+     * @param cod_usuario establece el codigo del usuario
+     */
+    public void setCod_usuario(int cod_usuario) {
+        this.cod_usuario = cod_usuario;
+    }
+    /**
+     * Es un Get que devuelve el usuario del usuario.
+     * 
+     * @return cod_usuario devuelve el usuario del usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+    /**
+     *Es un Set que establece el codigo del usuario.
+     * 
+     * @param usuario establece el usuario del usuario
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    /**
+     * Es un Get que devuelve la contraseña del usuario.
+     * 
+     * @return cod_usuario devuelve la contraseña del usuario
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+    /**
+     * Es un Set que establece el codigo del usuario.
+     * 
+     * @param contraseña establece la contraseña del usuario
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
     
 }
