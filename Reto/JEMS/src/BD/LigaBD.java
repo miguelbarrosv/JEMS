97a3d1c06ea1366a46600068612823ee5ab8f36d
eric <eric.munoz@ikasle.egibide.org>
2019-05-20 10:59:37 +0200

    cambios

--- Reto/JEMS/src/BD/LigaBD.java
+++ Reto/JEMS/src/BD/LigaBD.java
@@ -5,6 +5,9 @@
  */
 package BD;
 
+import java.sql.CallableStatement;
+import java.util.Date;
+
 /**
  * Clase de Liga de base de datos
  *
@@ -16,5 +19,42 @@ package BD;
  *
  */
 public class LigaBD {
-    
+
+    private static Bdr bdr;
+
+    public LigaBD() {
+        bdr = new Bdr();
+    }
+
+    /**
+     * Funcion que ejecuta el procedimiento PROC_REF_EQUIPO
+     *
+     * @return devuelve un string con todos los equipos con el nombre de su
+     * dueño y la cantidad de jugadores
+     * @throws Exception hereda excepciones
+     */
+    public String consultarTodosEquipos(Date fechaInicioLiga) throws Exception {
+        java.sql.Date sDate = convertUtilToSql(fechaInicioLiga);
+        bdr.conectar();
+        CallableStatement cStmt = bdr.getCon().prepareCall("{call PAQ_PROC_FUN.GENERAR_CALENDARIO(?)}");
+        cStmt.setDate(1, sDate);
+        cStmt.executeUpdate();
+        ResultSet rs = (ResultSet) cStmt.getObject(1);
+
+        rs.close();
+        cStmt.close();
+        bdr.cerrarCon();
+        return listaEquipos;
+    }
+
+    /**
+     * Transformar java date a sql date.
+     *
+     * @param fechaInicioLiga fecha del inicio de la liga
+     * @return devuelve la fecha en sql date
+     */
+    private static java.sql.Date convertUtilToSql(java.util.Date fechaInicioLiga) {
+        java.sql.Date sDate = new java.sql.Date(fechaInicioLiga.getTime());
+        return sDate;
+    }
 }
--- Reto/JEMS/src/Vistas/ControladorVistas.java
+++ Reto/JEMS/src/Vistas/ControladorVistas.java
@@ -32,10 +32,6 @@ public class ControladorVistas {
     private static V_Jornadas vJornadas; 
     private static V_Admin vAdmin;
     private static V_Usuario vUsuario;
-    private static V_Mostrar vMostrar;
-    private static V_Mostrar_Jugador vMostrarJugador;
-    private static V_Mostrar_Equipo vMostrarEquipo;
-    private static V_Mostrar_Dueño vMostrarDueño;
     private static V_Lista vLista;
     
  /**
@@ -235,62 +231,5 @@ public class ControladorVistas {
  */
     public static void cerrarVentanaUsuario() {
         vUsuario.dispose();
-    } 
-     /**
- * Funcion que nos dirije a la ventana V_Mostrar para seleccionar que queremos mostrar
- * 
- */
-    public static void mostrarVentanaMostrar() {
-        vMostrar.setVisible(true);
     }
-    /**
- * Funcion que cierra la ventana V_Mostrar
- * 
- */
-    public static void cerrarVentanaMostrar() {
-        vMostrar.dispose();
-    }  
-        /**
- * Funcion que abre la ventana V_Jugador
- * 
- */
-    public static void mostrarVentanaMostrarJugador() {
-        vMostrarJugador.setVisible(true);
-    }
-    /**
- * Funcion que cierra la ventana V_Jugador
- * 
- */
-    public static void cerrarVentanaMostrarJugador() {
-        vMostrarJugador.dispose();
-    } 
-        /**
- * Funcion que abre la ventana V_MostrarEquipo
- * 
- */
-    public static void mostrarVentanaMostrarEquipo() {
-        vMostrarEquipo.setVisible(true);
-    }
-    /**
- * Funcion que cierra la ventana V_MostrarEquipo
- * 
- */
-    public static void cerrarVentanaMostrarEquipo() {
-        vMostrarEquipo.dispose();
-    } 
-        /**
- * Funcion que abre la ventana V_MostrarDueño
- * 
- */
-    public static void mostrarVentanaMostrarDueño() {
-        vMostrarDueño.setVisible(true);
-    }
-    /**
- * Funcion que cierra la ventana V_MostrarDueño
- * 
- */
-    public static void cerrarVentanaMostrarDueño() {
-        vMostrarDueño.dispose();
-    } 
-    
 }
