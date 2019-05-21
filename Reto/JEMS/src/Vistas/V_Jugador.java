45e8715ea67a3817524f559a5f0198b0233d9b9f
Sergio <sergio.zulueta@ikasle.egibide.org>
2019-05-21 09:07:53 +0200

    Auto stash before merge of "master" and "origin/master"

--- Reto/JEMS/src/Vistas/V_Jugador.java
+++ Reto/JEMS/src/Vistas/V_Jugador.java
@@ -33,7 +33,7 @@ public class V_Jugador extends javax.swing.JFrame {
         initComponents();
         
     }
-    
+
     public void myInitComponents() {
         setSize(1280, 720);
         setLocationRelativeTo(null);
