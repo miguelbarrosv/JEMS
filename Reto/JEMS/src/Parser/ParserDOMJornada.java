d8094bd6c7f603bd593fc76f97e63058ff3a3205
Miguel <miguel.barros@ikasle.egibide.org>
2019-05-22 18:28:09 +0200

    parsers acabados

--- Reto/JEMS/src/Parser/ParserDOMJornada.java
+++ Reto/JEMS/src/Parser/ParserDOMJornada.java
@@ -12,6 +12,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -19,21 +21,52 @@ import org.xml.sax.SAXException;
  * @author Miguel Barros
  */
 public class ParserDOMJornada {
+
     private static Element elementoRaiz;
-    
+
     public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
         File archivo = new File("./jornada.xml");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            
+
         Document doc = dBuilder.parse(archivo);
         doc.getDocumentElement().normalize();
         System.out.println("Elemento raiz: " + doc.getDocumentElement().getNodeName());
-        
+
         elementoRaiz = doc.getDocumentElement();
-        System.out.println("cod: " + elementoRaiz.getAttribute("estado_liga"));
-        System.out.println("cod: " + elementoRaiz.getAttribute("fecha_fin"));
-        System.out.println("cod: " + elementoRaiz.getAttribute("fecha_inicio"));
-        System.out.println("cod: " + elementoRaiz.getAttribute("cod"));
+
+        NodeList listaJornadas = doc.getElementsByTagName("jornada");
+        System.out.println("------------------------");
+
+        for (int i = 0; i < listaJornadas.getLength(); i++) {
+            Node jornada = listaJornadas.item(i);
+            System.out.println("Elemento " + jornada.getNodeName());
+
+            if (jornada.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) jornada;
+                System.out.println("Fecha Fin: " + element.getAttribute("fecha_fin"));
+                System.out.println("Fecha Inicio: " + element.getAttribute("fecha_inicio"));
+                System.out.println("cod: " + element.getAttribute("cod"));
+                System.out.println("partidos: " + element.getElementsByTagName("partidos").item(0).getTextContent());
+            }
+
+            NodeList listaPartidos = doc.getElementsByTagName("partido");
+            System.out.println("-----------------------");
+
+            for (int x = 0; x < listaPartidos.getLength(); x++) {
+                Node partido = listaPartidos.item(x);
+                System.out.println("Elemento " + partido.getNodeName());
+
+                if (partido.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) partido;
+                    System.out.println("cod: " + element.getAttribute("cod"));
+                    System.out.println("Fecha Fin: " + element.getAttribute("fecha_fin"));
+                    System.out.println("Fecha Inicio: " + element.getAttribute("fecha_inicio"));
+                    System.out.println("Equipo Local: " + element.getElementsByTagName("equipo_local").item(0).getTextContent());
+                    System.out.println("Equipo Visitante: " + element.getElementsByTagName("equipo_visitante").item(0).getTextContent());
+                    System.out.println("Resultado: " + element.getElementsByTagName("resultado").item(0).getTextContent());
+                }
+            }
+        }
     }
 }
